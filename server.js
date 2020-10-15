4080bff76452eb557674316d5d1dbde91efec45f
JD Martinez <focus4ursoul@gmail.com>
2020-10-15 10:02:10 -0700

    save notes into db.json and display note list

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+let savedNotes = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get("/api/notes", (req, res) => {
+   res.json(savedNotes);
+});
+
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+//   });
+
+app.post("/api/notes", (req, res) => {
+    var newNote = req.body;
+
+    savedNotes.push(newNote);
+
+    fs.writeFile("./data/db.json", JSON.stringify(savedNotes), (err) => {
+        if (err) throw (err);
+    });
+
+    res.send(savedNotes);
+});
 
 app.listen(PORT, () => {
     console.log("The server is listening on PORT: http://localhost:" + PORT);
